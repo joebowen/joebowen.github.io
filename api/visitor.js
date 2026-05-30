4141f84c79aa9bec8a1ff6198357813830afb0c3
bjmichae <joe.bowen13@gmail.com>
2026-05-30 14:44:48 -0400

    domain change

--- api/visitor.js
+++ api/visitor.js
@@ -1,6 +1,14 @@
 import crypto from "crypto";
 
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "https://joebowen.github.io");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   if (req.method !== "POST" && req.method !== "GET") {
     return res.status(405).json({
       error: "Method not allowed"
